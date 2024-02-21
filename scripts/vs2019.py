f8d7928ad7ee296fefd170d808776f6596b9404a
Ivan Alekseev <puki@muki.com>
2024-02-21 21:51:35 +0100

    remove python dependency, updated readme

--- scripts/vs2019.py
+++
@@ -1,7 +0,0 @@
-import os
-
-GENERATOR = 'Visual Studio 16 2019'
-
-os.system(f'cmake -G "{GENERATOR}" -S .. -B ../_builds/vs2019')
-
-input("Press Enter to continue...")
--- scripts/vs2022.py
+++
@@ -1,7 +0,0 @@
-import os
-
-GENERATOR = 'Visual Studio 17 2022'
-
-os.system(f'cmake -G "{GENERATOR}" -S .. -B ../_builds/vs2022')
-
-input("Press Enter to continue...")
