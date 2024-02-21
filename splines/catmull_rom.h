fe6ade13ba40b5aaec8c5223ca418663a01ac11f
Ivan Alekseev <puki@muki.com>
2024-02-21 21:38:00 +0100

    better catmullrom implementation

--- splines/catmull_rom.h
+++ splines/catmull_rom.h
@@ -5,7 +5,7 @@
 
 namespace vanyka {
 
-template <class V, bool PassAllSupportPoint = false>
+template <class V, bool PassAllSupportPoint = true>
 class CatmullRomSpline : public Spline<V>
 {
 	static V Interpolate(float u, const V &P0, const V &P1, const V &P2, const V &P3);
@@ -34,17 +34,13 @@ std::vector<V> CatmullRomSpline<V, PassAllSupportPoint>::GeneratePoints(int res)
 		return points;
 
 	if (PassAllSupportPoint) {
-		V endPoints[2] = {
-			(mSupportPoints[0] * 3 - mSupportPoints[1])/ 2,
-			(mSupportPoints[mSupportPoints.size() - 1] * 3 - mSupportPoints[mSupportPoints.size() - 2]) / 2
-		};
 		points.reserve((mSupportPoints.size() - 1) * res);
 		points.push_back(mSupportPoints[0]);
 		for (int i = 2; i <= mSupportPoints.size(); ++i) {
-			const V& v1 = i - 3 < 0 ? endPoints[0] : mSupportPoints[i - 3];
+			const V& v1 = i - 3 < 0 ? mSupportPoints[0] : mSupportPoints[i - 3];
 			const V& v2 = mSupportPoints[i - 2];
 			const V& v3 = mSupportPoints[i - 1];
-			const V& v4 = i == mSupportPoints.size() ? endPoints[1] : mSupportPoints[i];
+			const V& v4 = i == mSupportPoints.size() ? mSupportPoints[mSupportPoints.size() - 1] : mSupportPoints[i];
 			for (int j = 1; j < res; ++j) {
 				const float t = (float)j / (float)res;
 				points.push_back(Interpolate(t, v1, v2, v3, v4));
