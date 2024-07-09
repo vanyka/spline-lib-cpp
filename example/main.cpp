cc1741b737c14ed41393951c1156e0d89545b855
vanyka <99076092+vanyka@users.noreply.github.com>
2024-07-09 10:36:22 +0200

    extended spline methods

--- example/main.cpp
+++ example/main.cpp
@@ -56,14 +56,21 @@ int main(){
 
 	std::unique_ptr<Spline<Vector2f>> curve = std::make_unique<CatmullRomSpline<Vector2f>>();
 
-	std::vector<Vector2f> points;
+	/*std::vector<Vector2f> points;
 	points.push_back({ 3.f, 2.f });
 	points.push_back({ -2.f, 6.f });
 	points.push_back({ 12.f, 12.f });
 	points.push_back({ 10.f, 14.f });
 	points.push_back({ 5.f, 19.f });
+	curve->AddSupportPoints(points.begin(), points.end());*/
 
-	curve->AddSupportPoints(points.begin(), points.end());
+	curve->AddSupportPoints<5>(new Vector2f[]{
+		{ 3.f, 2.f },
+		{ -2.f, 6.f },
+		{ 12.f, 12.f },
+		{ 10.f, 14.f },
+		{ 5.f, 19.f }
+	});
 
 	std::vector<Vector2f> res;
 	for (float t = 0.f; t <= 1.f; t += 1.f / 100.f) {
